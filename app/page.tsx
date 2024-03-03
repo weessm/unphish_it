3314dbe9c092e27d40e90e66a7a10cd3cf82d798
Weslei Miranda <wsmiranda11@gmail.com>
2024-03-03 00:12:59 -0300

    chore: setup project

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="h-screen bg-slate-700 flex gap-10 flex-col justify-center items-center">
+      <h1 className="text-5xl font-bold text-green-600">Unphish It</h1>
+      <p className="text-2xl text-white">O projeto está sendo atualizado</p>
+    </div>
+  );
+}
